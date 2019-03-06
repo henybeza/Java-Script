b9d608ee61a83e2b9d5bbb51a71258e4f2923228
henybeza <37804795+henybeza@users.noreply.github.com>
2019-03-06 23:14:31 +0300

    Second commit

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 function calculator(){
-let num1 = parseInt(prompt('Please enter the first number for calculations'));
+let num1 = parseInt(prompt('Enter the first number for calculations'));
 let op = prompt('Enter the operator');
-let num2 = parseInt(prompt('Please enter the second number for calculations'));
+let num2 = parseInt(prompt('Enter the second number for calculations'));
 
 if (op === '+'){alert(num1 + num2);}
 else if (op === '-'){alert(num1 - num2);}
